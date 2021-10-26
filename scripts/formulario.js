cd73f2d25452cf0b90e438029fa8d59b9ebe3afc
Santiago Montoya Cano <santiagomontoya98@hotmail.com>
2021-10-25 22:43:57 -0500

    Formulario + POST

---
+++ scripts/formulario.js
@@ -0,0 +1,44 @@
+let $formulario = document.forms[0],
+  url = "http://localhost:4000/games",
+  $nombre,
+  $precio,
+  $desc,
+  $categoria,
+  $arrayCategoria = [],
+  objeto;
+
+const capturarDatos = () => {
+  $nombre = $formulario.nombre.value,
+  $precio = $formulario.precio.value,
+  $desc = $formulario.desc.value,
+  $categoria = Array.from(document.querySelectorAll(".categoria"));  
+
+  $categoria.forEach(elemento => {
+    if(elemento.checked) $arrayCategoria.push(elemento.value);
+  });
+
+  objeto = {
+    title: $nombre,
+    category: $arrayCategoria,
+    price: $precio,
+    img: "",
+    desc: $desc
+  }
+  
+}
+
+const crearVideojuego = async (url) => {
+  await fetch(url,{
+    method: 'POST',
+    body: JSON.stringify(objeto),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8"
+    }
+  })
+}
+
+$formulario.addEventListener("submit", e => {
+  e.preventDefault();
+  capturarDatos();  
+  crearVideojuego(url);
+})
